ee6c3b545ac0b22e3713f12062febdbf056042d2
danis21 <92550075+danis21@users.noreply.github.com>
2021-10-15 01:57:40 +0300

    add the type-type game made in unity

---
+++ type-tpye/Scripts/LoseCondition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LoseCondition : MonoBehaviour {
+    int count = 0;
+    public GameObject text;
+    public GameObject spawner;
+    void OnTriggerEnter2D(Collider2D other)
+    {    count++;
+        if (count == 3) { Destroy(other.gameObject); GameOver(); }
+        else
+        {
+            Destroy(other.gameObject);
+
+        }
+        Debug.Log(count);
+    }
+    public void GameOver()
+    {
+        Debug.Log("Game Over!!!");
+        text.gameObject.SetActive(true);
+        spawner.gameObject.SetActive(false);
+    }
+}
---
+++ type-tpye/Scripts/NextScene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextScene : MonoBehaviour {
+
+    public float autoLoadNextLevelAfter;
+
+    private void Start()
+    {
+        if(autoLoadNextLevelAfter!=0)Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+    }
+
+    public void LoadLevel(string name)
+    {
+        Debug.Log("New Level load: " + name);
+        Application.LoadLevel(name);
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("Quit requested");
+        Application.Quit();
+    }
+
+    public void LoadNextLevel()
+    {
+        Application.LoadLevel(Application.loadedLevel + 1);
+    }
+
+}
